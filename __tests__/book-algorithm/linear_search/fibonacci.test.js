a43ed4865e3799b063f1f6b2b90cfdebed85b4d6
vertigo0538 <vertigo0538@gmail.com>
2023-03-07 09:24:50 +0900

    feat: 피보나치 수열 메모이제이션 버전

---
+++ __tests__/book-algorithm/linear_search/fibonacci.test.js
@@ -0,0 +1,11 @@
+describe("피보나치 함수", () => {
+  function fibonacci(n, memo) {
+    memo = memo || {}
+    if (memo[n]) return memo[n]
+    if (n <= 2) return 1;
+    return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+  }
+  test("피보나치", () => {
+    expect(fibonacci(100)).toEqual(55);
+  });
+});
---
+++ tests/book-algorithm/test_fibonacci.py
@@ -0,0 +1,27 @@
+
+# __fibo_cache = {}
+
+
+# def fibonacci(n):
+#     if n in __fibo_cache:
+#         return __fibo_cache[n]
+#     else:
+#         __fibo_cache[n] = n if n < 2 else fibonacci(n-2) + fibonacci(n-1)
+#         return __fibo_cache[n]
+
+fibo_cache = {}
+
+
+def fibonacci(number):
+    if number in fibo_cache:
+        return fibo_cache[number]
+    if number <= 2:
+        return 1
+    fibo_cache[number] = fibonacci(number - 1) + fibonacci(number - 2)
+    return fibo_cache[number]
+
+
+def test_fibonacci():
+    expected = 20
+    actual = fibonacci(50)
+    assert expected == actual
