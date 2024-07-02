0c507e6546ac17aa692019db9ec307c78e6122ff
vertigo0538 <vertigo0538@gmail.com>
2024-07-02 10:12:00 +0900

    fix: 소수 판별 함수 조건 추가

--- __tests__/book-algorithm/linear_search/is_prime.test.js
+++ __tests__/book-algorithm/linear_search/is_prime.test.js
@@ -1,11 +1,13 @@
 describe("소수를 판별하는 함수", () => {
   function is_prime(number) {
+    if (number <= 1) return false
+    if (number === 2) return true
     for (let i = 2; i < number; i++) {
       if (number % i === 0) return false;
     }
     return true;
   }
   test("is_prime", () => {
-    expect(is_prime(29)).toBe(true);
+    expect(is_prime(2)).toBe(true);
   });
 });
--- tests/book-algorithm/test_prime.py
+++ tests/book-algorithm/test_prime.py
@@ -1,4 +1,8 @@
 def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
     for i in range(2, number):
         if number % i == 0:
             return False
